679447ca6e9b0c7996d0a4af63849bf7998c8d9f
Vlad Korneev <veenrokdalv@protonmail.com>
2021-07-06 16:26:08 +0300

    v0.6.1b

--- app/data/types/files.py
+++ app/data/types/files.py
@@ -2,17 +2,13 @@ import os
 from datetime import datetime
 
 from aiogram.types import InputFile
-from loguru import logger
-from openpyxl import load_workbook, Workbook
+from openpyxl import Workbook
 
 
-class ExсelFile:
+class BaseTmpFile:
     __path_to_dir_tmp = './app/data/tmp'
 
     def __init__(self, filename=None, extension=None):
-        self.data: dict
-        self.book = Workbook()
-        self.sheet = self.book.active
         self.filename = self._format_filename(filename)
         self.extension = extension or 'xlsx'
         self.path_to_file = f'{self.__path_to_dir_tmp}/{self.filename}.{self.extension}'
@@ -20,20 +16,32 @@ class ExсelFile:
         with open(self.path_to_file, 'w+') as file:
             pass
 
+    @property
+    def input_file(self) -> InputFile:
+        return InputFile(self.path_to_file, self.filename)
+
     def _format_filename(self, filename):
         if not filename:
             return str(datetime.utcnow()).replace(".", ":").replace(" ", "_")
         return filename.replace(".", ":").replace(" ", "_")
 
+    def __del__(self):
+        os.remove(self.path_to_file)
+
+
+class ExсelFile(BaseTmpFile):
+
+    def __init__(self, filename=None, extension=None):
+        super().__init__()
+        self.data: dict
+        self.book = Workbook()
+        self.sheet = self.book.active
+
     def _set_columns_name(self, data: dict):
         pass
 
-
     def write_data(self, **data):
         self.book.save(self.path_to_file)
-
-    def get_file(self) -> InputFile:
-        return InputFile(self.path_to_file, self.filename)
 
     def __del__(self):
         self.book.close()
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from app import dp
 from app.utils.bot import on_startup, on_shutdown
 
 
+
 def main():
     executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
 
