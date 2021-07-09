b36575a8722fba22a672b429c440b8b13b4d2627
Vlad Korneev <veenrokdalv@protomail.com>
2021-07-09 20:54:15 +0300

    v0.6.0 Таблица промокодов

---
+++ app/utils/db_api/models/promocode_model.py
@@ -0,0 +1,18 @@
+import datetime as dt
+from sqlalchemy import BigInteger, Column, Sequence, String, Boolean
+
+from app.utils.db_api.db import BaseModel
+
+
+class Procode(BaseModel):
+    id: int = Column(BigInteger, Sequence('promocode_id'), primary_key=True)
+    code: str = Column(String(36), primary_key=True)
+    premium_datetime: str = Column(String(51))
+    user_id: int = Column(BigInteger)
+    creator_user_id: int = Column(BigInteger)
+    is_active: bool = Column(Boolean)
+
+    @property
+    def premium_timedelta(self) -> dt.timedelta:
+        days, hours = map(int, self.premium_datetime)
+        return dt.timedelta(days=days, hours=hours)
