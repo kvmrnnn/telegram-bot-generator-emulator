f8a72f486182b492c45910a809eb3f95adf9e2e0
Exiss <vladbelykh2002@gmail.com>
2021-07-14 19:32:09 +0300

    0.15.3 fix

--- app/handlers/private/default/callback_query/balance/check_payment.py
+++ app/handlers/private/default/callback_query/balance/check_payment.py
@@ -11,7 +11,7 @@ from app.utils.qiwi_api import qiwi_wallet
 async def check_payment(call: CallbackQuery, callback_data: dict, lang_code, user):
     comment = callback_data.get('comment')
     payment = await qiwi_wallet.get_transaction(comment)
-    if not payment:
+    if payment:
         await call.answer(
             show_alert=True,
             cache_time=60,
@@ -21,5 +21,6 @@ async def check_payment(call: CallbackQuery, callback_data: dict, lang_code, use
             )
         )
         return False
-    await user.update_balance(payment.total.amount)
     await call.answer(text=text[lang_code].message.default.balance_replenished, cache_time=900)
+    await user.update_balance(payment.total.amount)
+    await call.message.delete()
